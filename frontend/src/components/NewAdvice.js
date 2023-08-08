747968807a3bb883e8a818f6f2f0b0bd078180ef
Prathamesh-0 <79001922+Prathamesh-0@users.noreply.github.com>
2023-08-09 02:12:34 +0530

    Update NewAdvice.js

--- frontend/src/components/NewAdvice.js
+++ frontend/src/components/NewAdvice.js
@@ -65,6 +65,7 @@ const NewAdvice = () => {
 
         headers: {
           "Content-type": "application/json; charset=UTF-8",
+          'Access-Control-Allow-Origin': 'true'
         },
       })
         .then((response) => {
